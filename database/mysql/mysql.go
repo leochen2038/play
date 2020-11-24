4dfad49a82e9e9871ed4fe5d665708cee11b7fde
SCAUCJB <543965800@qq.com>
2020-11-24 16:10:27 +0800

    Update mysql.go
    add mysql query

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -60,6 +60,40 @@ func GetList(dest interface{}, query *play.Query) (err error) {
 	return
 }
 
+func QueryMap(sqlStr string, query *play.Query) (result []map[string]string, err error) {
+	var conn *sql.DB
+	var rows *sql.Rows
+	if conn, err = getConnect(query.Router); err != nil {
+		return
+	}
+	rows, err = conn.Query(sqlStr)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	//获取列名
+	columns, _ := rows.Columns()
+
+	//定义一个切片,长度是字段的个数,切片里面的元素类型是sql.RawBytes
+	values := make([]sql.RawBytes, len(columns))
+	//定义一个切片,元素类型是interface{} 接口
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		//把sql.RawBytes类型的地址存进去了
+		scanArgs[i] = &values[i]
+	}
+	//获取字段值
+	for rows.Next() {
+		res := make(map[string]string)
+		rows.Scan(scanArgs...)
+		for i, col := range values {
+			res[columns[i]] = string(col)
+		}
+		result = append(result, res)
+	}
+	return
+}
+
 func GetOne(dest interface{}, query *play.Query) (err error) {
 	var conn *sql.DB
 	var rows *sql.Rows
