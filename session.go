179030d7d3aaf91d35e35f8d7749e5026c2cbf89
Leo <leochen2038@gamil.com>
2020-12-24 22:04:47 +0800

    session接口

--- session.go
+++ session.go
@@ -4,4 +4,8 @@ type Session interface {
 	Logon(ctx *Context) error
 	Get(key string) (interface{}, error)
 	Set(key string, val interface{})
+	GetString(key string) (string, error)
+	SetString(key string, val string)
+	GetInt(key string) (int error)
+	SetInt(key string, val int)
 }
